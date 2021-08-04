7cae62d2d98a8341a2b7363114739641a38bd6ec
constantino_dev <constantino_dev@Constantinos-MacBook-Pro-2.local>
2021-08-03 22:06:48 -0500

    added basic functionality to interact with the contract

--- src/js/app.js
+++ src/js/app.js
@@ -15,7 +15,7 @@ App = {
         nftTemplate.find('.nft-date').text(data[i].date);
         nftTemplate.find('.nft-price').text(data[i].price);
         
-        nftTemplate.find('.btn-transfer').attr('data-id', data[i].id);
+        nftTemplate.find('.btn-buy').attr('data-id', data[i].id);
         nftTemplate.find('.btn-mint').attr('data-id', data[i].id);
 
         nftsRow.append(nftTemplate.html());
@@ -26,33 +26,145 @@ App = {
   },
 
   initWeb3: async function() {
-    
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      App.web3Provider = window.ethereum;
+      try{
+        // Request account access
+        await window.ethereum.enable();
+      } catch(error){
+        // User denied account access...
+        console.error("User denied account access")
+      }
+    }
+
+    // Legacy dapp browsers...
+    else if(window.web3) {
+      App.web3Provider = window.web3.currentProvider;
+    }
+
+    // If no injected web3 instance is detected, fall back to Ganache
+    else {
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    }
+
+    web3 = new Web3(App.web3Provider);
+
+    return App.initContract();
   },
 
   initContract: function() {
-    
+    $.getJSON('MyNFT.json', function(data) {
+      // Get the necessary contract artifact file and instantiate it with @truffle/contract
+      var MyNFTArtifact = data;
+      App.contracts.MyNFT = TruffleContract(MyNFTArtifact);
+
+      // Set the provider for our contract
+      App.contracts.MyNFT.setProvider(App.web3Provider);
+
+      // Use our contract to retrieve and mark the minted and sold NFTs
+      App.markMinted();
+      App.markSold();
+
+      return 0;
+      
+    });
+
+    return App.bindEvents();
   },
 
   bindEvents: function() {
-    
+    $(document).on('click', '.btn-buy', App.handleBuy);
+    $(document).on('click', '.btn-mint', App.handleMint);
   },
 
-  markTransferred: function() {
+  markSold: function() {
+    var nftInstance;
     
+    App.contracts.MyNFT.deployed().then(function(instance) {
+      nftInstance = instance;
+
+      // Get owners list from smart contract
+      return nftInstance.getOwners.call();
+    }).then(function(owners) {
+      for (i=0; i < owners.length; i++){
+        // Search for the ones assigned to mark them sold
+        if (owners[i] !== '0x0000000000000000000000000000000000000000') {
+          $('.panel-nft').eq(i).find('.btn-buy').text('Sold').attr('disabled', true);
+        }
+      }
+    }).catch(function(error) {
+      console.log(error.message);
+    });
   },
 
   markMinted: function() {
+    var nftInstance;
     
+    App.contracts.MyNFT.deployed().then(function(instance) {
+      nftInstance = instance;
+
+      // Get the minted NFTs list 
+      return nftInstance.getMinted.call();
+    }).then(function(minted) {
+      for (i=0; i < minted.length; i++){
+        // Search for the ones assigned to mark them Minted
+        if (minted[i] !== '0x0000000000000000000000000000000000000000') {
+          $('.panel-nft').eq(i).find('.btn-mint').text('Minted').attr('disabled', true);
+        }
+      }
+    }).catch(function(error) {
+      console.log(error.message);
+    });
   },
 
   handleMint: function(event) {
+    event.preventDefault();
+
+    var nftId = parseInt($(event.target).data('id'));
+    var nftInstance;
+
+    web3.eth.getAccounts(function(error, accounts) {
+      if(error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+      App.contracts.MyNFT.deployed().then(function(instance) {
+        nftInstance = instance;
+        
+        return nftInstance.mintNFT(nftId, {from: account});
+      }).then(function(result) {
+        return App.markMinted();
+      }).catch(function(error) {
+        console.log(error.message);
+      });
+    });
 
   },
 
-  handleTransfer: function(event) {
-    
-  },
+  handleBuy: function(event) {
+    event.preventDefault();
+    var nftId = parseInt($(event.target).data('id'));
+    var nftInstance;
 
+    web3.eth.getAccounts(function(error, accounts) {
+      if(error){
+        console.log(error);
+      }
+
+      var account = accounts[0];
+      App.contracts.MyNFT.deployed().then(function(instance) {
+        nftInstance = instance;
+        
+        return nftInstance.transferNFT(nftId, {from: account});
+      }).then(function(result) {
+        return App.markSold();
+      }).catch(function(error) {
+        console.log(error.message);
+      });
+    });    
+  }
 };
 
 $(function() {
